d6533d8b0ff2cc082ead4f550df9fc9d3afe4ea1
Allan <allankosgei747@gmail.com>
2022-03-07 15:37:44 +0300

    foo test

---
+++ tests/foo.specs.js
@@ -0,0 +1,8 @@
+// tests/foo.spec.ts
+import { test, expect } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
