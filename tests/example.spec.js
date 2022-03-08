7b6b43cb8b0b5d3d0bedf75240a33f3a951ee4bb
Allan <allankosgei747@gmail.com>
2022-03-08 18:20:09 +0300

    ab title test

---
+++ tests/example.spec.js
@@ -0,0 +1,15 @@
+// example.spec.js
+const { test, expect } = require('@playwright/test');
+
+test('my test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/abtest');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/The Internet/);
+  // Expect an attribute "to be strictly equal" to the value.
+  
+
+  
+
+  
+});
