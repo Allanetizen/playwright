8edec07e46cbc89d0c95165115c5d28de93cc446
Allan <allankosgei747@gmail.com>
2022-03-08 13:33:33 +0300

    added test for confirmation with ok clicked

--- tests/jsalerts.spec.js
+++ tests/jsalerts.spec.js
@@ -10,6 +10,15 @@ test.describe('Alert', () => {
     });
     await page.click('text=Click for JS Alert');
   });
-
+  test('should trigger a confirmation with a message', async ({ page }) => {
+    page.on('dialog', async dialog => {
+      expect(dialog.message()).toContain('I am a JS Confirm');
+      await dialog.accept();
+    });
+    await page.click('text=Click for JS Confirm');
+    const $answer = await page.$('#result');
+    const answerText = await $answer.innerText();
+    expect(answerText).toContain('You clicked: Ok');
+  });
 
 });
