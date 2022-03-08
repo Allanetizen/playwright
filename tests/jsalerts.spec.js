40c61b5a44f1ae1364fead5a82e703eda48466d7
Allan <allankosgei747@gmail.com>
2022-03-08 13:37:07 +0300

    commented

--- tests/jsalerts.spec.js
+++ tests/jsalerts.spec.js
@@ -1,24 +1,30 @@
-const { test, expect } = require('@playwright/test');
-test.describe('Alert', () => {
+const { test, expect } = require("@playwright/test");
+test.describe("Alert", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+    await page.goto("https://the-internet.herokuapp.com/javascript_alerts");
   });
-  test('should trigger an alert with a message', async ({ page }) => {
-    page.on('dialog', async dialog => {
-      expect(dialog.message()).toContain('I am a JS Alert');
+  test("should trigger an alert with a message", async ({ page }) => {
+    page.on("dialog", async (dialog) => {
+      
+      //alert dialog
+      expect(dialog.message()).toContain("I am a JS Alert");
+      
+      //dismiss dialog
       await dialog.dismiss();
     });
-    await page.click('text=Click for JS Alert');
+    await page.click("text=Click for JS Alert");
   });
-  test('should trigger a confirmation with a message', async ({ page }) => {
-    page.on('dialog', async dialog => {
-      expect(dialog.message()).toContain('I am a JS Confirm');
+  test("should trigger a confirmation with a message", async ({ page }) => {
+    page.on("dialog", async (dialog) => {
+      //assertion for confirm message
+      expect(dialog.message()).toContain("I am a JS Confirm");
       await dialog.accept();
     });
-    await page.click('text=Click for JS Confirm');
-    const $answer = await page.$('#result');
+    //click button
+    await page.click("text=Click for JS Confirm");
+    const $answer = await page.$("#result");
     const answerText = await $answer.innerText();
-    expect(answerText).toContain('You clicked: Ok');
+    //confirmation assertion
+    expect(answerText).toContain("You clicked: Ok");
   });
-
 });
