5d367290bd864cd34657ecd82c067c15cff3b52b
Allan <allankosgei747@gmail.com>
2022-03-09 00:26:30 +0300

    dropdown

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,29 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Select', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/dropdown');
+  });
+
+  test('should select  option 1 from the select dropdown', async ({ page }) => {
+      //select dropdown
+    await page.selectOption('#dropdown', '1');
+    
+    //set value of selected 
+    const selectedValue = await page.$eval('#dropdown', e => e.value);
+
+    //assertion  value = 1
+    expect(selectedValue).toContain('1');
+  });
+
+  test('should select  option 2 from the select dropdown', async ({ page }) => {
+      //select dropdown
+    await page.selectOption('#dropdown', '');
+
+    //set value of selected
+    const selectedValue = await page.$eval('#dropdown', e => e.value);
+    
+    //assertion  value = 2
+    expect(selectedValue).toContain('2');
+  });
+});
