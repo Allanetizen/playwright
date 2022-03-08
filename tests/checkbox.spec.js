ccd1d2421086bfdd1555109c9ec347044cdca4ca
Allan <allankosgei747@gmail.com>
2022-03-09 02:25:55 +0300

    checkbox passed

---
+++ tests/checkbox.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Checkboxes', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/checkboxes');
+  });
+
+  test('should check all the checkboxes', async ({ page }) => {
+     
+     
+     //check all the checkboxes 
+     const checkboxStates = await page.evaluate(() => {
+        const checkboxes = document.querySelectorAll('[type=checkbox]');
+        return [...checkboxes].map(c => c.checked);
+    });
+    
+
+    //assertion  value = 1
+    expect(checkboxStates).toBeTruthy();
+  });
+  test('should uncheck all the checkboxes', async ({ page }) => {
+     
+     
+    //uncheck all the checkboxes 
+    const checkboxStates = await page.evaluate(() => {
+       const checkboxes = document.querySelectorAll('[type=checkbox]');
+       return [...checkboxes].map(c => c.unChecked);
+   });
+   
+
+   //assertion  value = 1
+   expect(checkboxStates).toBeTruthy();
+ });
+
+
+  
+});
