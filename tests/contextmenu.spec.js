c3ca111b7075cf8c185802fdc81a2069b42126cf
Allan <allankosgei747@gmail.com>
2022-03-09 15:12:09 +0300

    updated

---
+++ tests/contextmenu.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require("@playwright/test");
+
+test('test', async ({ page }) => {
+  // Go to https://the-internet.herokuapp.com/context_menu
+  await page.goto('https://the-internet.herokuapp.com/context_menu');
+  // Click h3:has-text("Context Menu")
+  await page.locator('h3:has-text("Context Menu")').click();
+
+  // Click #hot-spot
+  await page.locator('#hot-spot').click();
+  // Click #hot-spot
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+
+    //assert dialog message
+  expect(dialog.message()).toContain("You selected a context menu");
+  });
+  
+  await page.locator('#hot-spot').click({
+    button: 'right'
+  });
+});
