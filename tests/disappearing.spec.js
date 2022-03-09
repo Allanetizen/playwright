bfbad4bae32f56037516927fee56dcebd9ac2f66
Allan <allankosgei747@gmail.com>
2022-03-09 21:50:50 +0300

    disappearing.js

--- tests/disappearing.spec.js
+++ tests/disappearing.spec.js
@@ -1,15 +1,20 @@
-const { test, expect } = require("@playwright/test");
+ const { test, expect } = require("@playwright/test");
 
 test('disappearing elements home', async ({ page }) => {
   // Go to https://the-internet.herokuapp.com/disappearing_elements
   await page.goto('https://the-internet.herokuapp.com/disappearing_elements');
   // Click text=Home
   await page.locator('text=Home').click();
+  //assert
+
+  
   await expect(page).toHaveURL('https://the-internet.herokuapp.com/');
   
  
-
+  
 
 
 });
+
+
 
