058104b961e315860c8c7aaf6be1ee6a827b85ce
Allan <allankosgei747@gmail.com>
2022-03-09 04:19:47 +0300

    added add element page test

---
+++ tests/addelete.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Add/Remove Elements", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com/add_remove_elements/");
+  });
+
+  test("should check all the checkboxes", async ({ page }) => {
+ 
+    
+    // Clicks add button.
+await page.locator('button:has-text("Add Element")').click();
+
+
+//expect a delete button
+expect(page.locator('.added-manually')).toBeTruthy();
+  });
+ 
+});
