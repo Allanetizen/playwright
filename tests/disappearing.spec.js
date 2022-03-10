d970f1587da62e01a26e0c5d6bb4c3bfff08a66d
Allan <allankosgei747@gmail.com>
2022-03-10 09:33:47 +0300

    added assertions to dynamic page and disappearing page

--- tests/disappearing.spec.js
+++ tests/disappearing.spec.js
@@ -1,20 +1,57 @@
- const { test, expect } = require("@playwright/test");
 
-test('disappearing elements home', async ({ page }) => {
-  // Go to https://the-internet.herokuapp.com/disappearing_elements
-  await page.goto('https://the-internet.herokuapp.com/disappearing_elements');
-  // Click text=Home
-  await page.locator('text=Home').click();
-  //assert
+const { test, expect } = require("@playwright/test");
+
+
+test.describe("DisappearingDynamic page test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/disappearing_elements');
+  });
+
+  test('should route to home page', async ({ page }) => {
+    
+  
+    await page.locator('text=Home').click();
+    //assert
+  
+    
+    await expect(page).toHaveURL('https://the-internet.herokuapp.com/');
 
   
-  await expect(page).toHaveURL('https://the-internet.herokuapp.com/');
+  });
   
- 
+  test('should route to about page', async ({ page }) => {
+    
+  //about button
+    await page.locator('text=About').click();
+    
+  //assert about
+  await expect(page).toHaveURL('https://the-internet.herokuapp.com/about/');
+  
+  
+    
+  
+  });
+
+
+  test('should route to contact us page', async ({ page }) => {
+    
+    //about button
+      await page.locator('text=Contact Us').click();
+      
+    //assert about
+    await expect(page).toHaveURL('https://the-internet.herokuapp.com/contact-us/');
+    
+    
+      
+    
+    });
   
 
 
+
+
+
+
+  
 });
-
-
 
--- tests/dynamic.spec.js
+++ tests/dynamic.spec.js
@@ -1,30 +1,43 @@
 const { test, expect } = require("@playwright/test");
 
 
-test('test', async ({ page }) => {
+test.describe("Dynamic page test", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/dynamic_content');
+  });
 
-  // Go to https://the-internet.herokuapp.com/dynamic_content
-  await page.goto('https://the-internet.herokuapp.com/dynamic_content');
-
-  // Click text=Dynamic Content
-  await page.locator('text=Dynamic Content').click();
-
-  // Click text=?with_content=static
-  await page.locator('text=?with_content=static').click();
-
-  // Click text=click here
-  await page.locator('text=click here').click();
-  await expect(page).toHaveURL('https://the-internet.herokuapp.com/dynamic_content?with_content=static');
-
-  // Click img >> nth=1
-  await page.locator('img').nth(1).click();
-
-  // Click img >> nth=2
-  await page.locator('img').nth(2).click();
-
-  // Click img >> nth=3
-  await page.locator('img').nth(3).click();
-
+  test('link test test', async ({ page }) => {
+    
+  
+    // Click text=Dynamic Content
+    await page.locator('text=Dynamic Content').click();
+  
+    // Click text=?with_content=static
+    await page.locator('text=?with_content=static').click();
+  
+    // Click text=click here
+    await page.locator('text=click here').click();
+    await expect(page).toHaveURL('https://the-internet.herokuapp.com/dynamic_content?with_content=static');
   
 
+  
+  });
+  
+  test('all images should have a true value', async ({ page }) => {
+    
+  
+    // Click img >> nth=1
+    await page.locator('img').nth(1).click();
+    
+  
+    
+    //assert all images
+    await expect(page.locator('img')).toBeTruthy()
+  
+  
+    
+  
+  });
+  
 });
+
